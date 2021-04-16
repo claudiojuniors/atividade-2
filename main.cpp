ba1a01ec7070dba525f4d6f1e1d34bb534f03ed2
cláudio junior <>
2021-04-16 20:48:09 +0000

    o raciocinio anterior não etava dando certo

--- main.cpp
+++ main.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 #include <string.h>
+#include <string>
 using namespace std;
 
-bool Busca( char vetor[], char letra){
+bool Busca(char vetor[], char letra){
   for (int i=0; i<strlen (vetor); i++){
-    if (vetor[i]==letra)
     return true;
   }
-  return false;
+  return (int i ++);
+  
 }
 int main(){
 
   char Palavra[50];
-  char letra [0];
+  string letra [0];
  
   cout<<"Digite uma palavra"<<endl;
   cin >> Palavra;
 
   for (int i=0; i <strlen(Palavra); i++){
 
-    if (Busca(Palavra,'a')){
+    if (Busca(Palavra ,'a')){
       cout<< "#2 = 1" <<endl;
     }
 
     else if (Busca(Palavra,'b')){
-      cout<< "#2 = 1" <<endl;
+      cout<< "#2 = 2" <<endl;
     }
 
     else if (Busca(Palavra,'c')){
-      cout<< "#2 = 1"<<endl;
+      cout<< "#2 = 3"<<endl;
     }
   }
-  }
+}
