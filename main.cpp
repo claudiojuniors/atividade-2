92a0567dd86d327f4b548f07da19f07ead6bad2c
cláudio junior <>
2021-04-16 19:15:36 +0000

    começando a imprimir

--- main.cpp
+++ main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+#include <string.h>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+bool Busca( char vetor[], char letra){
+  for (int i=0; i<strlen (vetor); i++){
+    if (vetor[i]==letra)
+    return true;
+  }
+  return false;
 }
+int main(){
+
+  char Palavra[50];
+  char letra [0];
+ 
+  cout<<"Digite uma palavra"<<endl;
+  cin >> Palavra;
+
+  for (int i=0; i <strlen(Palavra); i++){
+
+    if (Busca(Palavra,'a')){
+      cout<< "#2 = 1" <<endl;
+    }
+
+    else if (Busca(Palavra,'b')){
+      cout<< "#2 = 1" <<endl;
+    }
+
+    else if (Busca(Palavra,'c')){
+      cout<< "#2 = 1"<<endl;
+    }
+  }
+  }
